b8cdaee6d22b9805220632b22c8cbb8b00fc7a73
soouup-dev <soouup93@gmail.com>
2026-06-23 16:54:22 +0900

    Edit config URL

--- widgets/vite.config.ts
+++ widgets/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
-const FULL_URL = "https://ecommerce-server.nomadcoders.workers.dev/";
+const FULL_URL = "https://ecommerce-server.soouup.workers.dev";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
