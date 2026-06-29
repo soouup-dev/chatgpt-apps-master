8b162adb6915415aa8e5f2d486d28d3eb2c6ba3e
soouup-dev <soouup93@gmail.com>
2026-06-29 15:15:48 +0900

    feat: vite url 변경

--- widgets/vite.config.ts
+++ widgets/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-const FULL_URL = "https://ecommerce-server.soouup.workers.dev";
+const FULL_URL = "https://storyboard-server.soouup.workers.dev";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
